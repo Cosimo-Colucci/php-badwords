3c26201094eec0c0e89ab1e5cc8d702cce7b48cf
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-07-03 08:58:46 +0200

    first commit and test of page with bug to display server

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>index</title>
+    </head>
+    <body>
+        <form  action="./server.php" method="GET">
+
+            <label for="username">Username:</label><br>
+            <input type="text" name="username"><br>
+
+            <textarea name="textarea" id="" cols="30" rows="10"></textarea>
+
+            <button type="submit">
+                invia
+            </button>
+            
+        </form> 
+    </body>
+</html>
+
---
+++ server.php
@@ -0,0 +1,21 @@
+<?php
+
+    $user = $_GET['username'];
+    $text = $_POST['textarea'];
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Server</title>
+    </head>
+    <body>
+        <p>
+            <?php echo $user; ?>
+        </p>
+        <p>
+            <?php echo $text; ?>
+        </p>
+    </body>
+</html>
